e5c0ea379c1e3c35b0ef09fd8696708e9221cf96
Kamil Kwiatkowski <kk2203768@gmail.com>
2022-04-23 15:35:39 +0200

    tabliczka mnozenia

---
+++ tabliczka/tab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int wiersz,kolumna;
+	cout<<"Podaj liczbę wierszy:";
+		cin>>wiersz;
+	cout<<"Podaj liczbę kolumn:";
+		cin>>kolumna;
+
+	for(int i=1;i<=wiersz;i++){
+		for(int j=1;j<=kolumna;j++){
+                cout<<i*j<<"\t";
+		}
+		cout<<endl;	
+	}
+	return 0;
+}
