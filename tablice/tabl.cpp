cb8d3685d338c920ccbc83c3663ed7038fe90299
Kamil Kwiatkowski <kk2203768@gmail.com>
2022-04-23 16:36:17 +0200

    tablica

--- tablice/tabl.cpp
+++ tablice/tabl.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int main(){
-	int tablica[10];
-	//tablica[0],tablica[1],tablica[2]...,tablica[9]
-	cout<<"Uzupełniamy ręcznie elementy tablicy"<<endl;
-	for(int i=0;i<10;i++){
-		cout<<"tablica["<<i<<"]=";
+	int rozmiar;
+	cout<<"Podaj rozmiar tablicy: ";
+	cin>>rozmiar;
+
+	int tablica[rozmiar];
+	cout<<"Uzupełniamy ręcznie elementy tablicy: "<<endl;
+
+	for(int i=0;i<rozmiar;i++)
+	{
+		cout<<"tab["<<i<<"]=";
 		cin>>tablica[i];
 	}
+     	cout<<"Wyświetlamy elementy tablicy: ";
+
+	for(int i=0;i<rozmiar;i++)
+		cout<<tablica[i]<<"\t";
 	return 0;
 }
