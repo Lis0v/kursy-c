561909bd0b0599dd08bb4a3d5940509e37ceba75
Kamil Kwiatkowski <kk2203768@gmail.com>
2022-04-30 14:13:30 +0200

    newliny i wgl

--- referencje/main.cpp
+++ referencje/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void wart_ref(int x,int &y);
+void wart_ref(int x,int &y); //deklaracja funkcji
 
 int main(){
 
@@ -10,16 +10,17 @@ int main(){
 	cin>>a;
 	cout<<"Podaj wartość zmiennej b: ";
 	cin>>b;
-	cout<<"Wypisujemy a = "<<a<<" oraz b = "<<b;
+	cout<<"\nWypisujemy a = "<<a<<" oraz b = "<<b;
 	wart_ref(a,b);
+	cout<<"\nWypisujemy po wywołaniu funkcji a = "<<a<<" oraz b = "<<b<<"\n";
 
 	return 0;
 }
 
 //definicja funkcji o nazwie wart_ref
 void wart_ref(int x, int &y){
-	cout<<"A teraz z funkcji x = "<<x<<" y = "<<y;
+	cout<<"\nA teraz z funkcji x = "<<x<<" y = "<<y;
 	x=100*x; //x*=100
 	y=200*y; //y*=200
-	cout<<"Ponownie w funkcji x = "<<x<<" oraz y = "<<y;
+	cout<<"\nPonownie w funkcji x = "<<x<<" oraz y = "<<y;
 }
