964e9aab4be8b15dbf68feca508046626b511735
umbro <kk2203768@gmail.com>
2022-05-07 15:38:25 +0200

    koniec kodu

--- obiektowe/2.cpp
+++ obiektowe/2.cpp
@@ -68,4 +68,34 @@ void wzorzec_auta::add_auto()
 	cin>>this->pojemnosc;
 	cout<<"Podaj wartość przyspieszenia od 0 do 100km/h (s): ";
 	cin>>this->przyspieszenie;
+	cout<<"Podaj spalanie auta (l): ";
+	cin>>this->spalanie;
+	cout<<"Podaj kolor samochodu: ";
+	cin>>this->kolor;
+	cout<<"Podaj cenę samochodu: ";
+	cin>>this->cena;
+}
 
+void wzorzec_auta::view_auto()
+{
+	cout<<endl<<"Twoje auto to: "<<endl<<endl;
+	cout<<"Marka auta: "<<this->marka<<endl<<"Model: "<<this->model<<endl;
+	cout<<"Rocznik twojego auta: "<<this->rocznik<<endl;
+	cout<<"Pojemność twojego auta: "<<this->pojemnosc<<" cm3"<<endl;
+	cout<<"Przyspieszenie twojego auta: "<<this->przyspieszenie<<" s"<<endl;
+	cout<<"Spalanie twojego auta: "<<this->spalanie<<" l"<<endl;
+	cout<<"Kolor twojego auta to:"<<this->kolor<<"."<<endl;
+	cout<<"cena auta wynosi "<<this->cena<<endl;
+}
+int main()
+{
+	wzorzec_auta auto1(1999),auto2(2000,8),auto3(2006),auto4("Fiat",2001,10);//int a=8.b;
+	wzorzec_auta auto5("BMW","X3",2000,3000,5,10,"biały",150000);
+	/*auto1.add_auto();
+	 * auto1.view_auto();
+	 * auto2.add_auto();
+	 * auto2.view_auto();
+	 */
+
+	return 0;
+}
