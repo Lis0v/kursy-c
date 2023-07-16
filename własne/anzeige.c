b11f31edd1ee4fb647a895f60b2af788896c69ff
Lis0v <61664701+Lis0v@users.noreply.github.com>
2023-07-16 21:01:14 +0200

    Update anzeige.c

--- własne/anzeige.c
+++ własne/anzeige.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-	int *wsk;
-
+int main(){
 	char chiffre[10], beruf[20], sort[40], zeit[40], aufgaben[100], alter[5], beginn[20], verdienst[20], kontakt[50];
-
 	char *parametry[9]={chiffre, beruf, sort, zeit, aufgaben, alter, beginn, verdienst, kontakt};
-
 	char *index[9]={"ID-Chiffre", "Beruf", "Arbeitsort", "Arbeitszeit", "Aufgaben", "Alter", "Beginn", "Verdienst", "Kontakt"};
 
-	for(int i=0;i<9;i++)
-	{
+	for(int i=0;i<9;i++){
 		printf("%s: ", index[i]);
 		fgets(parametry[i], 100, stdin);
 	}
@@ -19,7 +13,6 @@ int main()
 	FILE *f = fopen("Anzeige.txt", "w");
 
 	for(int j=0;j<9;j++)
-	fprintf(f, "%s: %s", index[j], parametry[j]);
-
+		fprintf(f, "%s: %s", index[j], parametry[j]);
 	fclose(f);
 }
